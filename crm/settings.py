79328e6d8b6bf8517ea587277b0e024c4f99ebee
MartinMwiti <martinmwiti007@gmail.com>
2020-08-15 02:04:13 +0300

    make it better

--- crm/settings.py
+++ crm/settings.py
@@ -133,13 +133,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
