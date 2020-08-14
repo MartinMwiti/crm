715ffa5861e328712d52a0c8ec995106ee0b4261
MartinMwiti <martinmwiti007@gmail.com>
2020-08-15 01:34:53 +0300

    make it better

--- crm/settings.py
+++ crm/settings.py
@@ -72,15 +72,22 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'crm',
+#         # 'USER': 'postgres',
+#         # 'PASSWORD': 'shadow',
+#         'HOST': 'localhost',
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crm',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'shadow',
-        'HOST': 'localhost',
-        "PORT": "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
